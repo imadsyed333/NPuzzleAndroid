61d6be6f7e2dfd33879aef3bb6742ea5c7c94c3f
imadsyed333 <64811136+imadsyed333@users.noreply.github.com>
2021-12-30 19:48:53 -0500

    Created Cell.java and add necessary methods

---
+++ app/src/main/java/com/example/npuzzle/Cell.java
@@ -0,0 +1,44 @@
+package com.example.npuzzle;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+
+@SuppressLint("ViewConstructor")
+public class Cell extends androidx.appcompat.widget.AppCompatButton {
+    private int num;
+    private final int row;
+    private final int col;
+
+    public Cell(Context context, int num, int row, int col) {
+        super(context);
+        setNum(num);
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public String toString() {
+        if (this.num == 0) {
+            return " ";
+        }
+        return Integer.toString(this.num);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void setNum(int num) {
+        this.num = num;
+        setClickable(num != 0);
+        this.setText(toString());
+    }
+}
