73f7965008fc7b83c33e94bae26b3639d1f68975
imadsyed333 <64811136+imadsyed333@users.noreply.github.com>
2021-12-31 03:20:01 -0500

    Added methods to check if the puzzle is solved and launch the appropriate win message, with an option to return to the main menu

--- app/src/main/java/com/example/npuzzle/BoardView.java
+++ app/src/main/java/com/example/npuzzle/BoardView.java
@@ -1,17 +1,25 @@
 package com.example.npuzzle;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.GridLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 @SuppressLint("ViewConstructor")
 public class BoardView extends GridLayout implements View.OnClickListener {
     private final Board board;
+    Context context;
+    AppCompatActivity app;
 
-    public BoardView(Context context, Board board, int size) {
+    public BoardView(Context context, Board board, int size, AppCompatActivity app) {
         super(context);
+        this.context = context;
         this.board = board;
+        this.app = app;
         setRowCount(size);
         setColumnCount(size);
         setOrientation(GridLayout.HORIZONTAL);
@@ -27,9 +35,27 @@ public class BoardView extends GridLayout implements View.OnClickListener {
         }
     }
 
+    protected boolean isPuzzleSolved() {
+        return this.board.isBoardSolved();
+    }
     @Override
     public void onClick(View v) {
         Cell tile = (Cell) v;
         this.board.move(tile);
+        if (isPuzzleSolved()) {
+            endGame();
+        }
+    }
+
+    public void endGame() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, AlertDialog.THEME_DEVICE_DEFAULT_DARK);
+        builder.setTitle("Yay! You solved the puzzle!");
+        builder.setPositiveButton("Return to main menu", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                app.finish();
+            }
+        });
+        builder.show();
     }
 }
